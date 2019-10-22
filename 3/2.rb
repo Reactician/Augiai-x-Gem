c69cdb2c2e366402374ac6640fc7401cbebe031c
Augiz <augis916@gmail.com>
2019-10-22 17:40:02 +0300

    3

---
+++ 3/2.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require "test/unit"
+
+num = []
+
+class Palyginimas
+	def skaiciai(num)
+		return num.min
+	end
+end
+
+while num.length < 3
+	puts 'iveskite skaicius: '
+	i = gets.chomp
+	num << i.to_i
+end
+
+puts 'maziausias: ', Palyginimas.new::skaiciai(num)
+
+class TestPalyginimas < Test::Unit::TestCase
+  def test_skaiciai
+    assert_equal(69, Palyginimas.new::skaiciai([69, 420, 422]) )
+  end
+end
---
+++ 3/3.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require "test/unit"
+
+puts "iveskite pirma krastine:"
+x = gets.to_f
+puts "iveskite antra krastine:"
+y = gets.to_f
+puts "iveskite trecia krastine:"
+z = gets.to_f
+
+class Trikampiai
+  def trikampis(x, y, z)
+    if x + y > z && x + z > y && y + z > x
+      f = (x + y + z) / 2
+      s = Math.sqrt(f * (f - x) * (f - y) * (f - z))
+      if x == y && y == z
+        return "lygiakrastis trikampis! plotas: #{s.round(2)}"
+      elsif x == y && x != z || x == z && x != y
+        return "lygiasonis trikampis! plotas: #{s.round(2)}"
+      else
+        return "ivairiakrastis! plotas: #{s.round(2)}"
+      end
+    else
+      return "trikampio sudarymas negalimas"
+    end
+  end
+end
+
+puts Trikampiai.new::trikampis(x, y, z)
+
+class TestTrikampiai < Test::Unit::TestCase
+  def test_trikampis
+    assert_equal("lygiakrastis trikampis! plotas: 7.48", Trikampiai.new::trikampis(5, 5, 5) )
+    assert_equal("lygiasonis trikampis! plotas: 12.0", Trikampiai.new::trikampis(5, 5, 6) )
+    assert_equal("ivairiakrastis! plotas: 14.7", Trikampiai.new::trikampis(5, 6, 7) )
+    assert_equal("trikampio sudarymas negalimas", Trikampiai.new::trikampis(5, 5, 11) )
+  end
+end
